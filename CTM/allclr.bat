c8814f3a80f42f2d93079eae42d33eada89f091d
venkataraoparimi <118175623+venkataraoparimi@users.noreply.github.com>
2023-02-20 15:54:39 +0530

    Create allclr.bat

---
+++ CTM/allclr.bat
@@ -0,0 +1,46 @@
+@ECHO OFF
+CLS
+ECHO [1;37;40m                        ANSI COLOR CHART
+ECHO                         ----------------
+ECHO.
+ECHO [1;37;40m                           FOREGROUND
+ECHO.
+ECHO.
+ECHO [0;37;40m                     30 31 32 33 34 35 36 37
+ECHO [0;37;40m                  40 [s
+FOR %%P IN (30 31 32 33 34 35 36 37) DO ECHO [u[0;%%P;40mXX[30;40m [s
+ECHO.
+ECHO [37;40m                  41 [s
+FOR %%P IN (30 31 32 33 34 35 36 37) DO ECHO [u[0;%%P;41mXX[30;40m [s
+ECHO.
+ECHO [37;40m                  42 [s
+FOR %%P IN (30 31 32 33 34 35 36 37) DO ECHO [u[0;%%P;42mXX[30;40m [s
+ECHO.
+ECHO [37;40m                  43 [s
+FOR %%P IN (30 31 32 33 34 35 36 37) DO ECHO [u[0;%%P;43mXX[30;40m [s
+ECHO.
+ECHO [37;40m                  44 [s
+FOR %%P IN (30 31 32 33 34 35 36 37) DO ECHO [u[0;%%P;44mXX[30;40m [s
+ECHO.
+ECHO [37;40m                  45 [s
+FOR %%P IN (30 31 32 33 34 35 36 37) DO ECHO [u[0;%%P;45mXX[30;40m [s
+ECHO.
+ECHO [37;40m                  46 [s
+FOR %%P IN (30 31 32 33 34 35 36 37) DO ECHO [u[0;%%P;46mXX[30;40m [s
+ECHO.
+ECHO [37;40m                  47 [s
+FOR %%P IN (30 31 32 33 34 35 36 37) DO ECHO [u[0;%%P;47mXX[30;40m [s
+
+
+ECHO [1;37;40m
+ECHO [10;10HB
+ECHO [11;10HA
+ECHO [12;10HC
+ECHO [13;10HK
+ECHO [14;10HG
+ECHO [15;10HR
+ECHO [16;10HO
+ECHO [17;10HU
+ECHO [18;10HN
+ECHO [19;10HD
+ECHO [23;1H
